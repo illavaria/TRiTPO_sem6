4b9f8e1cb4122bbf3c7a75235eba041c3abed3e1
Artemiy <86923859+Chembampul@users.noreply.github.com>
2023-04-02 18:13:17 +0300

    Update SystemInfo.h

--- source/Header Files/SystemInfo.h
+++ source/Header Files/SystemInfo.h
@@ -3,36 +3,36 @@
 #include "ProcessVectorTreatment.h"
 #include <string>
 
-class SystemInfo {
+class SystemInfo {                                  //класс, в полях которого хранится общая информация о системе
 protected:
-    string model;
-    int coresNumber;
-    int totalRAM;
-    int totalRAMUsage;
-    vector<int> cpuUsage;
+    string model;                                   //модель процессора
+    int coresNumber;                                //количество ядер процессора
+    int totalRAM;                                   //общий объём ОЗУ
+    int totalRAMUsage;                              //используемый объём ОЗУ
+    vector<int> cpuUsage;                           //потребление ресурсов ЦПУ
 public:
     SystemInfo();
 
-    const string &getModel() const;
+    const string &getModel() const;                 //getters
     int getCoresNumber() const;
     int getTotalRam() const;
     int getTotalRamUsage() const;
     int getSingleCoreCpuUsage(int) const;
-    vector<int> getCpuUsage();
+    vector<int> getCpuUsage();                      //
 
-    void setModel(string);
+    void setModel(string);                          //setters
     void setCoresNumber(int);
     void setTotalRam(int);
     void setTotalRamUsage(int);
-    void setCpuUsage(vector<int>);
+    void setCpuUsage(vector<int>);                  //
 
-    string modelRecovery();
-    int coresNumberRecovery();
-    int totalRamRecovery();
-    int totalRamUsageRecovery();
-    //int singleCoreCpuUsageRecovery(int);
-    vector<int> cpuUsageRecovery();
-    void updateInfo();
+    string modelRecovery();                         //получение модели процессора
+    int coresNumberRecovery();                      //получения количества ядер ЦПУ
+    int totalRamRecovery();                         //получение общего объёма ОЗУ  
+    int totalRamUsageRecovery();                    //получение потребляемого объёма ОЗУ
+    //int singleCoreCpuUsageRecovery(int); 
+    vector<int> cpuUsageRecovery();                 //получение потребляемых ресурсов ЦПУ
+    void updateInfo();                              //метод актуализации информации в полях класса
     void outputInfo();
 };
 #endif // SYSTEMINFO_H
