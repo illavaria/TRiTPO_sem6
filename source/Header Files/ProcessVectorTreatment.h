14ba5be3b20104881998cfed59a3e58be3158b6d
Artemiy <86923859+Chembampul@users.noreply.github.com>
2023-04-02 18:58:26 +0300

    Update ProcessVectorTreatment.h

--- source/Header Files/ProcessVectorTreatment.h
+++ source/Header Files/ProcessVectorTreatment.h
@@ -10,30 +10,30 @@
 
 using namespace std;
 
-class ProcessVectorTreatment {
+class ProcessVectorTreatment {                                                  //класс, содержащий методы для взаимодействия со списком всех процессов
 protected:
-    vector<ProcessesInfo> processVector;
+    vector<ProcessesInfo> processVector;                                        //вектор всех процессов
 public:
     ProcessVectorTreatment();
-    void update_information();
-    void delete_process_vector();
-    void output_vector();
+    void update_information();                                                  //метод перезаполнения вектора актуальной информацией
+    void delete_process_vector();                                               //метод удаления вектора
+    void output_vector();                                                       //вывод вектора
 
-    void setProcessVector(vector<ProcessesInfo> setVect);
+    void setProcessVector(vector<ProcessesInfo> setVect);                       
     vector<ProcessesInfo> getProcessVector();
     vector<ProcessesInfo> findByName(string str);
 
-    void sortByName();
+    void sortByName();                                                          //сортировки вектора по параметрам
     void sortByPid();
     void sortByUser();
     void sortByThreadNumber();
-    void sortByMemory();
+    void sortByMemory();                                                        //
 
-    friend bool pidComparator(ProcessesInfo obj1, ProcessesInfo obj2);
+    friend bool pidComparator(ProcessesInfo obj1, ProcessesInfo obj2);         //компараторы для сортировок
     friend bool nameComparator(ProcessesInfo obj1, ProcessesInfo obj2);
     friend bool userComparator(ProcessesInfo obj1, ProcessesInfo obj2);
     friend bool threadsComparator(ProcessesInfo obj1, ProcessesInfo obj2);
-    friend bool memoryComparator (ProcessesInfo obj1, ProcessesInfo obj2);
+    friend bool memoryComparator (ProcessesInfo obj1, ProcessesInfo obj2);     //
 };
 
 
