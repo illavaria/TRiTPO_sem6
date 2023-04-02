0201a2ca70a2f8cff53d5ca531986eb4983492e5
Artemiy <86923859+Chembampul@users.noreply.github.com>
2023-04-02 17:49:56 +0300

    Update ProcessesInfo.h

--- source/Header Files/ProcessesInfo.h
+++ source/Header Files/ProcessesInfo.h
@@ -8,45 +8,45 @@
 
 using namespace std;
 
-class ProcessesInfo {
+class ProcessesInfo {                                   //класс, содержащий в своих полях информацию о процессах системы
 protected:
-    string commandLine;
-    string processName;
-    string user;
-    int threads;
-    int pid;
-    int memory;
-    double cpuPercent;
+    string commandLine;                                 //команда, запустившая процесс
+    string processName;                                 //имя процесса
+    string user;                                        //имя пользователя, запустившего процесс
+    int threads;                                        //количество дочерних потоков
+    int pid;                                            //идентификатор процесса в системе 
+    int memory;                                         //занимаемая процессом память
+    double cpuPercent;                                  //занимаемые ресурсы процессора
 
 public:
     ProcessesInfo();
     ProcessesInfo(int pid);
 
-    void setCommandLine(string str);
+    void setCommandLine(string str);                   //setters
     void setProcessName(string str);
     void setUser(string str);
     void setPid(int);
     void setThreads(int );
-    void setMemory(int);
+    void setMemory(int);                              //
 
-    const string &getCommandLine() const;
+    const string &getCommandLine() const;             //getters
     const string &getProcessName() const;
     const string &getUser() const;
     int getThreads() const;
     int getPid() const;
-    int getMemory() const;
+    int getMemory() const;                           //
 
-    string processNameRecovery();
-    string userRecovery();
-    string commandLineRecovery();
-    int memoryRecovery();
-    int threadsRecovery();
-    void infoOutput();
+    string processNameRecovery();                      //метод получения имени процесса
+    string userRecovery();                             //метод получения имени пользователя
+    string commandLineRecovery();                      //метод получения команды запуска
+    int memoryRecovery();                              //метод получения занимаемой памяти
+    int threadsRecovery();                             //метод получения количества дочерних потоков
+    void infoOutput();                                 //метод вывод всей информации
 
     ~ProcessesInfo();
 
 
 };
 
-int deleteProcess(int pid);
+int deleteProcess(int pid);                            //функция остановки процесса
 #endif // PROCESSESINFO_H
