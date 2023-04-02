607421797613d1d4a910947181184474b33fbbb7
Artemiy <86923859+Chembampul@users.noreply.github.com>
2023-04-02 16:47:28 +0300

    Update ConvertData.h

--- source/Header Files/ConvertData.h
+++ source/Header Files/ConvertData.h
@@ -7,14 +7,15 @@
 
 using namespace std;
 
-class ConvertData {
+class ConvertData {                                                     //класс содержит методы для преобразования одних типов данных в другие
+                                                                        //с целью упрощения работы с ними
 public:
     ConvertData();
-    static string itoa(int data);
-    static int atoi(string buf);
-    static string dtoa(double data, int fractional_part);
-    static string convertMemory(int data);
-    static string systemCall(const char* command);
+    static string itoa(int data);                                      //метод преобразования int в string
+    static int atoi(string buf);                                       //метод преобразования string в int
+    static string dtoa(double data, int fractional_part);              //метод преобразования double в string 
+    static string convertMemory(int data);                             //метод конвертации объёма памяти из байт в единицы измерения высших порядков(KB, MB, GB)
+    static string systemCall(const char* command);                     //метод для получения информации, возвращаемой системным вызовом, в формате string
 
 };
 #endif // CONVERTDATA_H
