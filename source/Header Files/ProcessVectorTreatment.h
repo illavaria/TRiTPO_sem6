9ef794bdbba8c24bd093db5a0efb764dd588cb5a
Artemiy <86923859+Chembampul@users.noreply.github.com>
2023-05-12 23:56:16 +0300

    Update ProcessVectorTreatment.h

--- source/Header Files/ProcessVectorTreatment.h
+++ source/Header Files/ProcessVectorTreatment.h
@@ -1,3 +1,101 @@
+/*ProcessVectorTreatment Class
+Header: #include<ProcessVectorTreatment.h>
+
+Public Functions
+                        ProcessVectorTreatment()
+void                    update_information()
+void                    delete_process_vector()
+void                    output_vector()
+void                    setProcessVector(vector<ProcessesInfo> setVect)
+vector<ProcessesInfo>   getProcessVector()
+vector<ProcessesInfo>   findByName(string str)
+
+void                    sortByName()
+void                    sortByPid()
+void                    sortByUser()
+void                    sortByThreadNumber()
+void                    sortByMemory()
+
+bool                    pidComparator(ProcessesInfo obj1, ProcessesInfo obj2)           friend
+bool                    nameComparator(ProcessesInfo obj1, ProcessesInfo obj2)          friend
+bool                    userComparator(ProcessesInfo obj1, ProcessesInfo obj2)          friend
+bool                    threadsComparator(ProcessesInfo obj1, ProcessesInfo obj2)       friend
+bool                    memoryComparator (ProcessesInfo obj1, ProcessesInfo obj2)       friend
+
+
+ProcessVectorTreatment::ProcessVectorTreatment()
+-------------------------------------------
+Метод создает объект типа ProcessVectorTreatment и заполняет поле vector<ProcessInfo> processVector
+
+void ProcessVectorTreatment::update_information()
+-------------------------------------------
+Метод перезаполнет поле vector<ProcessInfo> processVector актуальной информацией
+
+void ProcessVectorTreatment::delete_process_vector()
+-------------------------------------------
+Метод очищает поле vector<ProcessInfo> processVector
+
+void ProcessVectorTreatment::output_vector()
+-------------------------------------------
+Метод выводит содержимое поля vector<ProcessInfo> processVector
+
+void ProcessVectorTreatment::setProcessVector(vector<ProcessesInfo> setVect)
+-------------------------------------------
+Метод заполняет поле vector<ProcessInfo> processVector переданным в метод вектором vector<ProcessesInfo> setVect
+
+vector<ProcessesInfo>   ProcessVectorTreatment::getProcessVector()
+-------------------------------------------
+Метод возвращает содержимое поля vector<ProcessInfo> processVector в векторе типа vector<ProcessInfo>
+
+vector<ProcessesInfo>   ProcessVectorTreatment::findByName(string str)
+-------------------------------------------
+Метод возвращает в векторе типа vector<ProcessInfo> объекты, в которых поле processName совпадает с переданным полем str
+
+vector<ProcessesInfo>   ProcessVectorTreatment::findByName(string str)
+-------------------------------------------
+Метод возвращает в векторе типа vector<ProcessInfo> объекты, в которых поле processName совпадает с переданным полем str
+
+void   ProcessVectorTreatment::sortByName()
+-------------------------------------------
+Метод сортирует поле vector<ProcessInfo> processVector по полю processName объектов типа ProcessInfo находящихся в векторе processVector 
+
+void   ProcessVectorTreatment::sortByPid()
+-------------------------------------------
+Метод сортирует поле vector<ProcessInfo> processVector по полю pid объектов типа ProcessInfo находящихся в векторе processVector 
+
+void   ProcessVectorTreatment::sortByUser()
+-------------------------------------------
+Метод сортирует поле vector<ProcessInfo> processVector по полю user объектов типа ProcessInfo находящихся в векторе processVector 
+
+void   ProcessVectorTreatment::sortByTreadNumber()
+-------------------------------------------
+Метод сортирует поле vector<ProcessInfo> processVector по полю threads объектов типа ProcessInfo находящихся в векторе processVector 
+
+void   ProcessVectorTreatment::sortByMemory()
+-------------------------------------------
+Метод сортирует поле vector<ProcessInfo> processVector по полю memory объектов типа ProcessInfo находящихся в векторе processVector 
+
+bool   ProcessVectorTreatment::pidComparator()                  [friend]
+-------------------------------------------
+Метод компаратора, использующийся в методе sortByPid
+
+bool   ProcessVectorTreatment::nameComparator()                 [friend]
+-------------------------------------------
+Метод компаратора, использующийся в методе sortByName
+
+bool   ProcessVectorTreatment::userComparator()                 [friend]
+-------------------------------------------
+Метод компаратора, использующийся в методе sortByUser
+
+bool   ProcessVectorTreatment::threadsComparator()              [friend]
+-------------------------------------------
+Метод компаратора, использующийся в методе sortByThreadNumber
+
+bool   ProcessVectorTreatment::memoryComparator()               [friend]
+-------------------------------------------
+Метод компаратора, использующийся в методе sortByMemory
+*/
+
 #ifndef KURS_PROJECT_PROCESSVECTORTREATMENT_H
 #define KURS_PROJECT_PROCESSVECTORTREATMENT_H
 #include <iostream>
@@ -6,7 +104,6 @@
 #include <memory>
 #include <algorithm>
 #include "ProcessesInfo.h"
-
 
 using namespace std;
 
@@ -35,7 +132,4 @@ public:
     friend bool threadsComparator(ProcessesInfo obj1, ProcessesInfo obj2);
     friend bool memoryComparator (ProcessesInfo obj1, ProcessesInfo obj2);     //
 };
-
-
-
 #endif //KURSUPDATE_PROCESSVECTORTREATMENT_H
